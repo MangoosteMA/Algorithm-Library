96c113157a37f462d26bdbd8aa8d44b3be14a473
Алексей Михненко <alex@MacBook-Pro-Aleksej-2.local>
2023-03-03 00:51:41 +0300

    added strip

--- Helpers/stress.py
+++ Helpers/stress.py
@@ -14,7 +14,7 @@ while True:
 
     system('./a < test > wa 2> /dev/null')
     system('./brute < test > ok 2> /dev/null')
-    if open('ok', 'r').read() != open('wa', 'r').read():
+    if open('ok', 'r').read().strip() != open('wa', 'r').read().strip():
         print(f'wrong answer on test {test_num}:')
         system('cat test')
         print('\nyour answer:')
