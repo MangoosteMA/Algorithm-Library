5d623379edb76165baf211bbbb7d0b9cd7dd7941
Алексей Михненко <alex@MacBook-Pro-Aleksej-3.local>
2023-07-11 14:01:26 +0300

    Added small doc.

--- Data structures/binary_indexed_tree.cpp
+++ Data structures/binary_indexed_tree.cpp
@@ -1,39 +1,44 @@
+/*
+ * Type T must have operator += T.
+ * Type T must have default constructor, which sets neutral value.
+ * Operation += must be commutative.
+ * For segment query type T must have operator -= T.
+*/
+
 template<typename T>
 struct binary_indexed_tree {
     std::vector<T> bit;
 
-    binary_indexed_tree(int n = 0, T value = 0) : bit(n + 1) {
-        for (int i = 1; i <= n; i++)
-            bit[i] = value * (i & -i);
-    }
+    // Fills the array with default values.
+    binary_indexed_tree(int n = 0) : bit(n + 1) {}
 
+    // Returns size of the initial array.
     int size() const {
         return int(bit.size()) - 1;
     }
 
+    // Adds delta at the position pos (zero based).
     void add(int pos, T delta) {
         for (pos++; pos < int(bit.size()); pos += pos & -pos)
             bit[pos] += delta;
     }
 
+    // Returns sum on the interval [0, pref].
     T query(int pref) {
-        T sum = 0;
+        T sum = T();
         for (pref++; pref > 0; pref -= pref & -pref)
             sum += bit[pref];
 
         return sum;
     }
 
+    // Returns sum on the interval [l, r).
     T query(int l, int r) {
-        if (r >= int(bit.size()))
-            r = bit.size();
-
-        if (l < 0)
-            l = 0;
-
         if (r <= l)
-            return 0;
+            return T();
 
-        return query(r - 1) - query(l - 1);
+        T res = query(r - 1);
+        res -= query(l - 1);
+        return res;
     }
 };
