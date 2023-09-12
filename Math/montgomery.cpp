510a7fef5a108c816fc325cc60d637a5cd2e80f8
Алексей Михненко <alex@MacBook-Pro-Aleksej-3.local>
2023-09-12 15:14:44 +0300

    Bug fix

--- Math/montgomery.cpp
+++ Math/montgomery.cpp
@@ -33,7 +33,7 @@ public:
     montgomery(const mint &x) : value(x.value) {}
 
     template<typename T, typename U = std::enable_if_t<std::is_integral<T>::value>>
-    montgomery(const T &x) : value(!x ? 0 : reduce(int64_t(x % mod + mod) * neg_mod)) {}
+    montgomery(const T &x) : value(!x ? 0 : reduce(int64_t(x % int32_t(mod) + int32_t(mod)) * neg_mod)) {}
 
     uint32_t get() const {
         auto real_value = reduce(value);
