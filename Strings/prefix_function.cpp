52a03154626ac532430665206e8c0b13bb62aa48
Alexey Mikhnenko <AlexeyMikhnenko@yandex.ru>
2024-01-08 22:55:50 +0300

    Added small doc

--- Strings/prefix_function.cpp
+++ Strings/prefix_function.cpp
@@ -1,14 +1,20 @@
+/*
+  Input   |         Output
+"abacaba" | {0, 0, 1, 0, 1, 2, 3}
+{1, 1, 1} |       {0, 1, 2}
+*/
+
 template<typename T>
 std::vector<int> prefix_function(const T &str) {
-    int n = int(str.size());
+    int n = str.size();
     std::vector<int> p(n);
     for (int i = 1, j = 0; i < n; i++) {
-        while (j > 0 && str[i] != str[j])
+        while (j > 0 && str[i] != str[j]) {
             j = p[j - 1];
-
-        if (str[i] == str[j])
+        }
+        if (str[i] == str[j]) {
             j++;
-
+        }
         p[i] = j;
     }
     return p;
