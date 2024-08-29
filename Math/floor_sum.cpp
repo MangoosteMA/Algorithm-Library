6c4ec7b0163fdb5618d5076b6069f46d63b95c64
Alexey Mikhnenko <AlexeyMikhnenko@yandex.ru>
2024-08-29 23:07:31 +0300

    Doc fixed.

--- Math/floor_sum.cpp
+++ Math/floor_sum.cpp
@@ -1,6 +1,8 @@
-// Returns sum_{x = 0}^{n - 1} floor((kx + b) / m)
-// Require: k >= 0, b >= 0, m > 0, n >= 0
-// WARNING: careful with overflow. Don't forget to specify large enough type T.
+/*
+ ! WARNING: careful with overflow. Don't forget to specify large enough type T.
+ * Returns sum_{x = 0}^{n - 1} floor((kx + b) / m).
+ * Require: k >= 0, b >= 0, m > 0, n >= 0.
+ */
 template<typename T>
 T floor_sum(T k, T b, T m, T n) {
     if (k == 0) {
@@ -13,9 +15,11 @@ T floor_sum(T k, T b, T m, T n) {
     return n * ymax - floor_sum(m, m + k - b - 1, k, ymax);
 }
 
-// Returns sum_{x = 0}^{n - 1} (kx + b) % m
-// Require: m > 0, n >= 0
-// WARNING: careful with overflow. Don't forget to specify large enough to fit floor_sum type T.
+/*
+ ! WARNING: careful with overflow. Don't forget to specify large enough to fit floor_sum type T.
+ * Returns sum_{x = 0}^{n - 1} (kx + b) % m.
+ * Require: m > 0, n >= 0.
+ */
 template<typename T>
 T mod_sum(T k, T b, ll m, ll n) {
     k = (k % m + m) % m;
@@ -23,9 +27,11 @@ T mod_sum(T k, T b, ll m, ll n) {
     return n * (n - 1) / 2 * k + n * b - m * floor_sum(k, b, m, n);
 }
 
-// Returns the number of 0 <= x < n, such that (kx + b) mod m <= v
-// Require: m > 0, n >= 0
-// WARNING: careful with overflow. Don't forget to specify large enough to fit floor_sum type T.
+/*
+ ! WARNING: WARNING: careful with overflow. Don't forget to specify large enough to fit floor_sum type T.
+ * Returns the number of 0 <= x < n, such that (kx + b) mod m <= v.
+ * Require: m > 0, n >= 0.
+ */
 template<typename T>
 T count_remainders(T k, T b, T v, T m, T n) {
     if (v < 0) {
